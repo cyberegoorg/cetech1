93f961bda5815f68bd31b3aa8698761f164af04a
Ondra Voves <email@ondravoves.cz>
2023-09-27 17:01:58 +0200

    update zig to actual master

---
+++ externals/shared/bin/zig/doc/std/data-modules.js
@@ -0,0 +1,1 @@
+var modules =[{"name":"std","file":0,"main":66,"table":{"builtin":1,"root":2,"std":0}},{"name":"builtin","file":0,"main":436,"table":{}},{"name":"root","file":0,"main":9783,"table":{}}];
